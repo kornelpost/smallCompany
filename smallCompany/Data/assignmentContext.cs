166c8d4d5bf43f2649b1f2ae309ffd79aa627489
Kornel <post.kornel@gmail.com>
2022-03-23 19:36:01 +0100

    database context created

---
+++ smallCompany/Data/assignmentContext.cs
@@ -0,0 +1,21 @@
+﻿using smallCompany.Models;
+using Microsoft.EntityFrameworkCore;
+namespace smallCompany.Data
+{
+    public class assignmentContext : DbContext
+    {
+        public assignmentContext(DbContextOptions<assignmentContext> options) : base(options)
+        {
+        }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Assignment>().ToTable("Assignment");
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+            modelBuilder.Entity<Task>().ToTable("Task");
+        }
+    }
+    
+}
