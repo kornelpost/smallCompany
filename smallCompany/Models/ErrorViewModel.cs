0c23a1a36dbd89f8c6d79ffefa324a0c3d065f6a
Kornel <post.kornel@gmail.com>
2022-03-23 18:41:59 +0100

    initial

---
+++ smallCompany/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace smallCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
