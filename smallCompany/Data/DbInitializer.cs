a9048d1f1a70b623760b41787e9422c12534bd8d
Kornel <post.kornel@gmail.com>
2022-03-23 20:18:20 +0100

    database sample data ready

---
+++ smallCompany/Data/DbInitializer.cs
@@ -0,0 +1,50 @@
+﻿using smallCompany.Models;
+using System;
+using System.Linq;
+
+
+namespace smallCompany.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(assignmentContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Employees.Any())
+            {
+                return; // DB already fulfiffled with test data
+            }
+
+            var employees = new Employee[]
+            {
+                new Employee {FirstName = "Dawid", LastName = "Post"}
+            };
+            foreach (Employee e in employees)
+            {
+                context.Employees.Add(e);
+            }
+            context.SaveChanges();
+
+            var tasks = new Task[]
+            {
+                new Task { TaskName = "Remont glowicy Renault", Details = "Plaszczyzna, wymiana prowadnic komplet, szlif zaworów komplet"}
+            };
+            foreach (Task t in tasks)
+            {
+                context.Tasks.Add(t);
+            }
+            context.SaveChanges();
+
+            var assignments = new Assignment[]
+            {
+                new Assignment { EmployeeID = 1, TaskID = 1}
+            };
+            foreach (Assignment a in assignments)
+            {
+                context.Assignments.Add(a);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- smallCompany/Startup.cs
+++ smallCompany/Startup.cs
@@ -1,3 +1,5 @@
+using smallCompany.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +25,11 @@ namespace smallCompany
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<assignmentContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddControllersWithViews();
         }
 
